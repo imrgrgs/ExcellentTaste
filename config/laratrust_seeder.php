3683de8da61ac6bd25d9a4dc787c9c11b4453aae
Mark Wierda <97041029@st.deltion.nl>
2019-01-16 13:19:27 +0100

    Added Laratrust

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'role_structure' => [
+        'administrator' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ]
+    ],
+    'permission_structure' => [
+        'cru_user' => [
+            'profile' => 'c,r,u'
+        ],
+    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
