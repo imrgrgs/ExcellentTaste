794b3b26cc2c5439ab6758d547022700931b0183
Chiel Pieters <chieleman@hotmail.com>
2019-01-22 13:56:14 +0100

    Added menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Product;
+use App\User;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        $view = view('menu');
+        $view->categories = Category::all();
+
+        return $view;
+    }
+
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(TablesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
+        $this->call(MenuSeeder::class);
     }
 }
