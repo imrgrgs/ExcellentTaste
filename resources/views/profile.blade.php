65b397d78c2730a3f6e0556c0e3ccd0128fb096f
Chiel Pieters <chieleman@hotmail.com>
2019-01-24 13:05:42 +0100

    Bug fixes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,46 +12,59 @@
                             <div class="form-group row">
                                 <label for="seat" class="col-sm-4 col-form-label">Voornaam</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="first_name" class="form-control-plaintext" readonly id="seat" value="{{$user->first_name}}"/>
+                                    <input type="text" name="first_name" class="form-control-plaintext" readonly
+                                           id="seat" value="{{$user->first_name}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Tussenvoegsel</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="middle_name" class="form-control-plaintext" readonly id="seat" value="{{$user->middle_name}}"/>
+                                    <input type="text" name="middle_name" class="form-control-plaintext" readonly
+                                           id="seat" value="{{$user->middle_name}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Achternaam</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="last_name" class="form-control-plaintext" readonly id="seat" value="{{$user->last_name}}"/>
+                                    <input type="text" name="last_name" class="form-control-plaintext" readonly
+                                           id="seat" value="{{$user->last_name}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Adres</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="address" class="form-control-plaintext" readonly id="seat" value="{{$user->address}}"/>
+                                    <input type="text" name="address" class="form-control-plaintext" readonly id="seat"
+                                           value="{{$user->address}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Postcode</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="postal" class="form-control-plaintext" readonly id="seat" value="{{$user->postal}}"/>
+                                    <input type="text" name="postal" class="form-control-plaintext" readonly id="seat"
+                                           value="{{$user->postal}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Stad</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="city" class="form-control-plaintext" readonly id="seat" value="{{$user->city}}"/>
+                                    <input type="text" name="city" class="form-control-plaintext" readonly id="seat"
+                                           value="{{$user->city}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">E-mail</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="email" class="form-control-plaintext" readonly id="seat" value="{{$user->email}}"/>
+                                    <input type="text" name="email" class="form-control-plaintext" readonly id="seat"
+                                           value="{{$user->email}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Mobiel</label>
                                 <div class="col-sm-8">
-                                    <input type="text" name="phone" class="form-control-plaintext" readonly id="seat" value="{{$user->phone}}"/>
+                                    <input type="text" name="phone" class="form-control-plaintext" readonly id="seat"
+                                           value="{{$user->phone}}"/>
                                 </div>
                                 <label for="seat" class="col-sm-4 col-form-label">Klantnr.</label>
                                 <div class="col-sm-8">
-                                    <label type="text" name="number" class="form-control-plaintext" readonly id="seat">{{$user->number}}</label>
+                                    <label type="text" name="number" class="form-control-plaintext" readonly
+                                           id="seat">{{$user->number}}</label>
                                 </div>
                                 <div class="col-md-3">
-                                    <button type="button" data-edit="false" class="btn btn-secondary" id="editform">Wijzigen</button>
+                                    <button type="button" data-edit="false" class="btn btn-secondary" id="editform">
+                                        Wijzigen
+                                    </button>
                                     <button type="submit" class="btn btn-success" id="submit">Opslaan</button>
                                 </div>
                                 <div class="col-md-3">
-                                    <a href="/profile/delete" class="btn btn-danger" onclick="return confirm('Weet u zeker dat u uw account wilt verwijderen?');" id="">Verwijderen</a>
+                                    <a href="/profile/delete" class="btn btn-danger"
+                                       onclick="return confirm('Weet u zeker dat u uw account wilt verwijderen?');"
+                                       id="">Verwijderen</a>
                                 </div>
                             </div>
                         </form>
@@ -79,7 +92,7 @@
                                 </div>
                                 <div class="col-md-6 mt-2">
                                     <div class="g-recaptcha"
-                                            data-sitekey="{{env('GOOGLE_RECAPTCHA_KEY')}}">
+                                         data-sitekey="{{env('GOOGLE_RECAPTCHA_KEY')}}">
                                     </div>
                                 </div>
                                 <div class="col-md-8">
@@ -131,48 +144,46 @@
                                     &nbsp;
                                 </td>
                             </tr>
-                           </thead>
-                           @foreach($user->reservations as $reservation)
-                           <tr>
-                               <td>
-                                   {{$reservation->date_string}}
-                               </td>
-                               <td>
-                                   {{$reservation->tables->first()->start_time}} - {{$reservation->tables->first()->end_time}}
-                               </td>
-                               <td>
-                                   {{$reservation->tables->first()->id}}
-                               </td>
-                               <td>
-                                   {{$reservation->tables->first()->seat_count}}
-                               </td>
-                               <td>
+                            </thead>
+                            @foreach($user->reservations as $reservation)
+                                <tr>
+                                    <td>
+                                        {{$reservation->date_string}}
+                                    </td>
+                                    <td>
+                                        {{$reservation->tables->first()->start_time}} - {{$reservation->tables->first()->end_time}}
+                                    </td>
+                                    <td>
+                                        {{$reservation->tables->first()->id}}
+                                    </td>
+                                    <td>
+                                        {{$reservation->tables->first()->seat_count}}
+                                    </td>
+                                    <td>
                                    <span class="d-inline-block text-truncate" style="max-width: 150px;">
                                        <span data-toggle="tooltip" data-placement="bottom" title="{{$reservation->diet}}"> {{$reservation->diet}}</span>
                                    </span>
-                               </td>
-                               <td>
-                                   {{$reservation->number}}
-                               </td>
-                               <td>
-                                   @if(file_exists(storage_path('app/public/notas/'.$reservation->number.'.pdf')))
-                                        <a href="/reservations/{{$reservation->number}}/download-nota">{{$reservation->number}}.pdf</a>
-                                   @else
+                                    </td>
+                                    <td>
+                                        {{$reservation->number}}
+                                    </td>
+                                    <td>
+                                        @if(file_exists(storage_path('app/public/notas/'.$reservation->number.'.pdf')))
+                                            <a href="/reservations/{{$reservation->number}}/download-nota">{{$reservation->number}}.pdf</a>
+                                        @endif
+                                    </td>
+                                    @if($reservation->nota === 1)
+                                        <td>
 
-                                   @endif
-                               </td>
-                               @if($reservation->nota === 'false')
-                               <td>
-                                   <a href="/profile/{{$reservation->id}}/delete" class="btn btn-danger" onclick="return confirm('Weet u zeker dat u uw reservering wilt verwijderen?');" id="">Verwijderen</a>
-                               </td>@else
-                                   <td>
+                                        </td>@else
+                                        <td>
+                                            <a href="/profile/{{$reservation->id}}/delete" class="btn btn-danger" onclick="return confirm('Weet u zeker dat u uw reservering wilt verwijderen?');" id="">Verwijderen</a>
+                                        </td>
 
-                                   </td>
-
-                                   @endif
-                           </tr>
+                                    @endif
+                                </tr>
                             @endforeach
-                       </table>
+                        </table>
                     </div>
                 </div>
             </div>
