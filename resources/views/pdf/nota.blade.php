097ca791d3a809b3965dc7731e451d084d9cc19d
SilentSwaps <leroyveldhoen@gmail.com>
2019-01-24 11:15:33 +0100

    reciept styling.

--- resources/views/pdf/nota.blade.php
+++ resources/views/pdf/nota.blade.php
@@ -7,6 +7,7 @@
         <title>Document</title>
     </head>
     <body>
+        {{-- personal info --}}
         <ul>
             <li>{{ $reservation->user->name }}</li>
             <li>{{ $reservation->user->address }}</li>
@@ -18,28 +19,95 @@
             <li>0591-272012</li>
         </ul>
         <ul>
-            <li>Datum: {{ $reservation->date_string }}</li>
-            <li>Start: {{ $reservation->tables->first()->start_time }}</li>
-            <li>Eind: {{ $reservation->tables->first()->end_time }}</li>
+            <li>Datum van reservering: {{ $reservation->date_string }}</li>
+            <li>Starttijd: {{ $reservation->tables->first()->start_time }}</li>
+            <li>Eindtijd: {{ $reservation->tables->first()->end_time }}</li>
         </ul>
-        <table>
-            <thead>
-                <tr>
-                    <td>Product</td>
-                    <td>Hoeveelheid</td>
-                    <td>Betaalt</td>
-                </tr>
-            </thead>
+        {{-- Reciept --}}
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Product</th>
+              <th scope="col">Aantal</th>
+              <th scope="col">Prijs per stuk</th>
+              <th scope="col">Betaalt</th>
+            </tr>
+          </thead>
+          <tbody>
             @foreach($reservation->orders as $order)
                     @foreach($order->products as $product)
                     <tr>
-                        <td>{{ $product->name }}</td><td>{{ $product->pivot->amount }}</td><td>€ {{ number_format($product->pivot->payed, 2, ',', '.') }}</td>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->pivot->amount }}</td>
+                        <td>€ {{ number_format($product->pivot->payed/$product->pivot->amount, 2, ',', '.') }}</td>
+                        <td>€ {{ number_format($product->pivot->payed, 2, ',', '.') }}</td>
                     </tr>
                     @endforeach
             @endforeach
             <tr>
-                <td colspan="2">totaal:</td><td>€ {{ number_format($reservation->total_price, 2, ',', '.') }}</td>
+                <td></td><td></td><td></td><td></td>
             </tr>
+            <tr>
+                <td colspan="3">Totaal:</td><td>€ {{ number_format($reservation->total_price, 2, ',', '.') }}</td>
+            </tr>
+          </tbody>
         </table>
+        
+<style type="text/css">
+    ul{
+        list-style: none;
+    }
+    .table {
+        width: 100%;
+        max-width: 100%;
+        margin-bottom: 1rem;
+        background-color: transparent;
+    }
+
+    .table thead th {
+        vertical-align: bottom;
+        border-bottom: 2px solid #dee2e6;
+    }
+
+    .table td, .table th {
+        padding: .75rem;
+        vertical-align: top;
+        border-top: 1px solid #dee2e6;
+    }
+
+    table {
+        display: table;
+        border-collapse: separate;
+        border-spacing: 2px;
+        border-color: grey;
+    }
+
+    thead {
+        display: table-header-group;
+        vertical-align: middle;
+        border-color: inherit;
+    }
+
+    th {
+        text-align: inherit;
+    }
+
+    td, th {
+        display: table-cell;
+        vertical-align: inherit;
+    }
+
+    tbody {
+        display: table-row-group;
+        vertical-align: middle;
+        border-color: inherit;
+    }
+
+    tr {
+        display: table-row;
+        vertical-align: inherit;
+        border-color: inherit;
+    }
+</style>
     </body>
 </html>
